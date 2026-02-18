9dd998cdf8ee16e03a41163125f174e3156fd234
LordEddine <ahmedimededdinerabah@gmail.com>
2026-02-18 07:32:32 -0500

    fix: ajout type module dans package et .js

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,8 @@
 import express from 'express';
-import studentRoutes from './routes/studentRoutes'
-import clubRoutes from './routes/clubRoutes';
+import studentRoutes from './routes/studentRoutes.js'
+import clubRoutes from './routes/clubRoutes.js';
 import 'dotenv/config';
-import prisma from './prisma/prisma';
+import prisma from './prisma/prisma.js';
 import cors from 'cors';
 
 const app = express();
